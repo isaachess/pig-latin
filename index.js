456974d01320254850b4f5c3a5c23de6e521f155
Isaac Hess <isaachess@gmail.com>
2014-09-08 21:06:31 -0600

    All working but capitalization

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var _ = require('lodash')
 
-var vowels = ['a', 'e', 'i', 'o', 'u', 'y']
+var vowels = ['a', 'e', 'i', 'o', 'u']
+var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 function piggyAnyText(text, options) {
     var splitted = text.split(' ')
@@ -11,15 +12,19 @@ function piggyAnyText(text, options) {
 }
 
 function piggyOneWord(word) {
+    console.log('piggyOneWord',word)
+    var bigBacon
     var formatted = formatWord(word)
-    if (isVowel(formatted[0]))       // If first letter is a vowel
-        return piggyVowelWord(formatted)
+    if (isVowel(formatted.wordPart[0]))       // If first letter is a vowel
+        bigBacon = piggyVowelWord(formatted.wordPart)
     else
-        return piggyConsonantWord(formatted)
+        bigBacon = piggyConsonantWord(formatted.wordPart)
+    return bigBacon+formatted.puncPart
 }
 
 // Use this method to piggy a word that starts with a vowel
 function piggyVowelWord(word, options) {
+    console.log('piggyVowelWord',word, options)
     if (!options || !options.vowelEnding)   // If no options specified, do it MY way
         return word+'way'
     else                                    // Else return the specified option (but it's still the WRONG way to do it)
@@ -28,7 +33,7 @@ function piggyVowelWord(word, options) {
 
 // Method to piggy a word that starts w/ consonant
 function piggyConsonantWord(word) {
-
+    console.log('piggyConsonantWord',word)
     // Split the word into its two parts. I know this is ugly, but it works
     var vowelLocations = vowels.map(function(vowel) {
         return word.indexOf(vowel)
@@ -49,12 +54,29 @@ function reconstruct(piggified) {
 }
 
 function formatWord(word) {
+    console.log('formatWord',word)
     word = word.replace("'", "")
-    return word
+    var arrayOfLetters = word.split("")
+    console.log('arrayOfLetters',arrayOfLetters)
+    var letterLocs = arrayOfLetters.map(function(letter) {
+        return alphabet.indexOf(letter)
+    })
+    console.log('letterLocs',letterLocs)
+    var startOfPunc = letterLocs.indexOf(-1)
+    if (startOfPunc < 0) startOfPunc = word.length
+    var wordPart = word.slice(0, startOfPunc)
+    var puncPart = word.slice(startOfPunc, word.length)
+    var x = {
+        wordPart:wordPart,
+        puncPart:puncPart
+    }
+    console.log('x',x)
+    return x
 }
 
 function isVowel(letter) {
-    return !!_.find(vowels, function(vowel) {return vowel == letter.toLowerCase()})
+    console.log('isVowel',letter)
+    return !!_.find(vowels, function(vowel) {return vowel == letter})
 }
 
 module.exports = piggyAnyText
