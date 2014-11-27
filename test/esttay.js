e7c979744203e0e9342d2eadea7773eacb4156e0
Ray John Alovera <rakista112@gmail.com>
2014-11-27 11:20:57 +0800

    tested capitalization

--- test/esttay.js
+++ test/esttay.js
@@ -3,7 +3,11 @@ var should = chai.should();
 var pigLatin = require('./../index');
 
 describe('igpay-atinlay', function(){
-    it('igpay-atinlay ouldshay iggifypay ordsway', function(){
+    it('ouldshay iggifypay ordsway', function(){
 	pigLatin('piggified').should.equal('iggifiedpay');
+    });
+
+    it('ouldshay apitalizecay entencessay', function(){
+	pigLatin('oh why bother...', {capitalize: true}).should.equal('Ohway ywhay otherbay...');
     });
 });
