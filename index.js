e18d96b948636cc607c01a5d48f9b566e517ecfc
Isaac Hess <isaachess@gmail.com>
2015-04-22 08:10:13 -0600

    Minor refactors

--- index.js
+++ index.js
@@ -9,27 +9,25 @@ var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
 
 // This is the master method called in the module
 function piggyAnyText(text, options) {
-    var splitted,
-        piggified,
-        capitalized
-    splitted = prepForThePig(text)
-    piggified = splitted.map(function(word) {
+    var arrayOfWords = prepForThePig(text)
+    var piggified = arrayOfWords.map(function(word) {
         return piggyOneWord(word, options)
     })
     return reconstruct(piggified)
 }
 
 function piggyOneWord(word, options) {
-    var bigBacon,
-        formatted
-    formatted = formatWord(word)
-
+    var formatted = formatWord(word)
+    var bigBacon = piggyVowelOrConsonantWord(formatted.wordPart, options)
     // Send the word off to get piggified WITHOUT its punctuation
-    if (isVowel(formatted.wordPart[0].toLowerCase()))     // If first letter is a vowel
-        bigBacon = piggyVowelWord(formatted.wordPart, options)
-    else                                    // If first letter is consonant
-        bigBacon = piggyConsonantWord(formatted.wordPart)
     return bigBacon+formatted.puncPart      // Reattach punctuation when returning
+}
+
+function piggyVowelOrConsonantWord(wordPart, options) {
+    if (arrayStartsWithVowel(wordPart))     // If first letter is a vowel
+        return piggyVowelWord(wordPart, options)
+    else                                    // If first letter is consonant
+        return piggyConsonantWord(wordPart)
 }
 
 // Use this method to piggy a word that starts with a vowel
@@ -42,39 +40,29 @@ function piggyVowelWord(word, options) {
 
 // Method to piggy a word that starts w/ consonant
 function piggyConsonantWord(word) {
-    var vowelLocations,
-        firstVowelLocation,
-        wordWasCapitalized,
-        part1,
-        part2
-
-    // Check if the word was capitalized before piggifying
-    wordWasCapitalized = wasCapitalized(word)
 
     // Split the word into its two parts. I know this is ugly, but it works
     // First find all the vowels
-    vowelLocations = vowels.map(function(vowel) {
+    var vowelLocations = vowels.map(function(vowel) {
         return word.indexOf(vowel)
     })
     // Then find the location of the first vowel
-    firstVowelLocation = _.min(vowelLocations, function(location) {
+    var firstVowelLocation = _.min(vowelLocations, function(location) {
         if (location < 0)
             return 100
         else return location
     })
     // Slice, dice, and reconstruct word
-    part1 = word.slice(0, firstVowelLocation)
-    part2 = word.slice(firstVowelLocation, word.length)
+    var part1 = word.slice(0, firstVowelLocation)
+    var part2 = word.slice(firstVowelLocation, word.length)
 
     // If the word was capitalized before piggifying
-    if(wordWasCapitalized){
+    if (wasCapitalized(word)) {
 	// Set original first letter to lower case
 	part1 = part1.charAt(0).toLowerCase() + part1.slice(1)
-	
 	// Capitalize the first letter of the word
 	part2 = capitalizeWord(part2)
     }
-
     return part2+part1+'ay'
 }
 
@@ -83,40 +71,36 @@ function piggyConsonantWord(word) {
 //////////////////////////////////////
 
 function prepForThePig(text) {
-    text = text.replace(/-|–|—/g, ", ")     // Remove all dashes, because they are hecka hard to handle -- make them commas, instead
-    text = text.split(' ')
-    return text
+    var noDashes = text.replace(/-|–|—/g, ", ")     // Remove all dashes, because they are hecka hard to handle -- make them commas, instead
+    return noDashes.split(' ')
 }
 
 function formatWord(word) {
-    var arrayOfLetters,
-        letterLocs,
-        startOfPunc,
-        wordPart,
-        puncPart
-    word = word.replace("'", "")            // Remove apostrophes, because the piggy Latin don't need no stinkin' apostrophes!
+    var formatted = word.replace("'", "")            // Remove apostrophes, because the piggy Latin don't need no stinkin' apostrophes!
 
     // Split into an array of single letters
-    arrayOfLetters = word.split('')
+    var arrayOfLetters = formatted.split('')
     // Find all the locations of letters, as opposed to other symbols
-    letterLocs = arrayOfLetters.map(function(letter) {
+    var letterLocs = arrayOfLetters.map(function(letter) {
         return alphabet.indexOf(letter.toLowerCase())
     })
     // Find where the punctuation begins -- this assumes punctuation is always at the END of a word
-    startOfPunc = letterLocs.indexOf(-1)
-    if (startOfPunc < 0) startOfPunc = word.length
+    var startOfPunc = letterLocs.indexOf(-1)
+    var locationToUse = (startOfPunc < 0) ? word.length : startOfPunc
     // Return the word and its punctuation separately -- they'll be reattached at the very end
-    wordPart = word.slice(0, startOfPunc)
-    puncPart = word.slice(startOfPunc, word.length)
     return {
-        wordPart:wordPart,
-        puncPart:puncPart
+        wordPart: word.slice(0, locationToUse),
+        puncPart: word.slice(locationToUse, word.length)
     }
 }
 
 //////////////////////////////////////
 ///// Helper functions ///////////////
 //////////////////////////////////////
+
+function arrayStartsWithVowel(array) {
+    return isVowel(array[0].toLowerCase())
+}
 
 function reconstruct(piggified) {
     return piggified.join(' ')
